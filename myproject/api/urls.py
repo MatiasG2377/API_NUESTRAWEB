ef9438da1ebeceb26a972cd3f6c5af289399c509
Matías Gamboa <85884703+MatiasG2377@users.noreply.github.com>
2025-09-30 13:23:23 -0500

    a
    a

--- myproject/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import GaleriaViewSet, FechaViewSet
-
-router = DefaultRouter()
-router.register(r'galeria', GaleriaViewSet)
-router.register(r'fecha', FechaViewSet)
-
-urlpatterns = router.urls
