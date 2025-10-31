8243ac316303480c94e45846c0da2a283637da3a
Matías Gamboa <85884703+MatiasG2377@users.noreply.github.com>
2025-10-31 09:42:38 -0500

    Hacer el buil.sh un ejecutable

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
+# build.sh
+
+set -o errexit  # Detiene el script si ocurre un error
+
 pip install -r requirements.txt
-
+python manage.py collectstatic --noinput
 python manage.py migrate
-
